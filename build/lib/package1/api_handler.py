833c89973ed0ef2692f9db740189a752e95d57ad
Mikołaj Kamiński <241388@student.pwr.edu.pl>
2020-05-24 20:12:28 +0200

    added wheel

---
+++ build/lib/package1/api_handler.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import time
+from . import bibtex_classes
+from requests.exceptions import HTTPError
+
+class Manager:
+    def test(self):
+        return "It actually works"
